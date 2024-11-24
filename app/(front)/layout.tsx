71f990da4e09dd39c9fa8b6fea006555528cb047
Ahmed Ibrahim <ahmedibrahimhassan654@gmail.com>
2024-11-25 00:49:48 +0200

    create nave bar

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,17 +1,5 @@
+import SiteHeader from "@/components/site-header";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "FrontLayout",
@@ -24,12 +12,9 @@ export default function FrontLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <SiteHeader />
+      {children}
+    </>
   );
 }
